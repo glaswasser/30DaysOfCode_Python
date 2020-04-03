8d9af764b642ce81df085c2ec54531026dd40c3e
glaswasser <glaswasser@gmail.com>
2020-04-03 21:12:04 +0200

    added description

---
+++ Day_10_Binary_Numbers.py
@@ -0,0 +1,26 @@
+# this is the first file I imported using git and my terminal
+# yayyy!! :) 
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input())
+    assert 1 <= n <= 10**6
+    binary = bin(n)[2:]
+    counter = 0
+    saved = int()
+    for i in range(0, len(binary)):
+        if binary[i] == "1":
+            counter +=1
+        if binary[i] == "0":
+            counter = 0
+        if counter > saved:
+            saved = counter
+
+print(saved)
